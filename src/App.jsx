993f2584108c314133af7d5a1704c8a7b010dfde
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-24 09:28:32 -0300

    Resgatando dados da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // Etapa 1- Recuperando os dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+    fetchData()
+  }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
